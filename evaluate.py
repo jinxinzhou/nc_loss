2a194852b0e3da7a474f3450c2196827adf9dcaf
Jinxin Zhou <48702671+jinxinzhou@users.noreply.github.com>
2022-05-10 00:43:06 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,6 @@ import pickle
 import argparse
 import scipy.linalg as scilin
 from torch import nn
-import matplotlib.pyplot as plt
 import torch.backends.cudnn as cudnn
 
 # Import dataloaders
@@ -17,16 +16,9 @@ import Data.tiny_imagenet as tiny_imagenet
 
 # Import network architectures
 from Net.resnet_tiny_imagenet import resnet50 as resnet50_ti
-from Net.resnet import resnet50, resnet110
-from Net.wide_resnet import wide_resnet_cifar
+from Net.resnet import resnet50, resnet110, wide_resnet50_cifar
+
 from Net.densenet import densenet121
-
-# Import metrics to compute
-from Metrics.metrics import test_classification_net_logits
-from Metrics.metrics import ECELoss, AdaptiveECELoss, ClasswiseECELoss
-
-# Import temperature scaling and NLL utilities
-from temperature_scaling import ModelWithTemperature
 
 from train_utils import AverageMeter, compute_accuracy, print_and_save
 
@@ -48,9 +40,19 @@ models = {
     'resnet50': resnet50,
     'resnet50_ti': resnet50_ti,
     'resnet110': resnet110,
-    'wide_resnet': wide_resnet_cifar,
+    'wide_resnet50': wide_resnet50_cifar,
     'densenet121': densenet121
 }
+
+def set_seed(manualSeed=666):
+    random.seed(manualSeed)
+    np.random.seed(manualSeed)
+    torch.manual_seed(manualSeed)
+    torch.cuda.manual_seed(manualSeed)
+    torch.cuda.manual_seed_all(manualSeed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ['PYTHONHASHSEED'] = str(manualSeed)
 
 
 def split_array(input_array, batchsize=512):
@@ -121,7 +123,7 @@ def compute_Sigma_W(before_class_dict, mu_c_dict, device, batchsize=256):
         class_feature_list = split_array(np.array(before_class_dict[target]), batchsize=batchsize)
         for features in class_feature_list:
             features = torch.from_numpy(features).to(device)
-            Sigma_W_batch = (features - mu_c_dict[target].unsqueeze(0)).unsqueeze(2) * (
+            Sigma_W_batch = (features - mu_c_dict[target].unsqueeze(0)).unsqueeze(2) @ (
                     features - mu_c_dict[target].unsqueeze(0)).unsqueeze(1)
             Sigma_W += torch.sum(Sigma_W_batch, dim=0)
             num_data += features.shape[0]
@@ -221,8 +223,8 @@ def compute_margin(before_class_dict, after_class_dict, W, b, mu_G, device, batc
 
 def parseArgs():
     default_dataset = 'cifar100'
-    dataset_root = '~/'
-    model = 'wide_resnet'
+    dataset_root = '../'
+    model = 'wide_resnet50'
     width = 10
     epoch = 400
     save_interval = epoch // 100
@@ -246,7 +248,7 @@ def parseArgs():
                         dest="model_name", help='name of the model')
     parser.add_argument("--model", type=str, default=model, dest="model",
                         help='Model to test')
-    parser.add_argument("--width", type=int, default=width, dest="width",
+    parser.add_argument("--width", type=float, default=width, dest="width",
                         help='Width of wide-resnet')
     parser.add_argument("--bias", action="store_true", dest="bias",
                         help="Whether to use bias in fc of network")
@@ -307,7 +309,7 @@ if __name__ == "__main__":
     args = parseArgs()
 
     # Setting additional parameters
-    torch.manual_seed(args.seed)
+    set_seed(args.seed)
     device = torch.device("cuda" if cuda else "cpu")
 
     if args.model_name is None:
@@ -365,7 +367,6 @@ if __name__ == "__main__":
         'ETF_metric': [],
         'WH_relation_metric': [],
         'Wh_b_relation_metric': [],
-        #         'nuclear_metric': [],
         'avg_cos_margin': [],
         'all_cos_margin': [],
         'W': [],
@@ -377,42 +378,21 @@ if __name__ == "__main__":
         'mu_c_dict_train': [],
         'mu_c_dict_val': [],
         'mu_c_dict_test': [],
-        #         'before_class_dict_train': [],
-        #         'after_class_dict_train': [],
-        #         'before_class_dict_val': [],
-        #         'after_class_dict_val': [],
-        #         'before_class_dict_test': [],
-        #         'after_class_dict_test': [],
         'train_acc1': [],
         'train_acc5': [],
         'val_acc1': [],
         'val_acc5': [],
         'test_acc1': [],
         'test_acc5': [],
-        'best_val_test_prev_acc': None,
-        'best_val_test_prev_conf_matrix': None,
-        'best_val_test_prev_ece': None,
-        'best_val_test_prev_adaece': None,
-        'best_val_test_prev_cece': None,
-        'best_val_test_prev_nll': None,
-        'best_val_test_prev_all_cosM': None,
-        'best_val_test_post_acc': None,
-        'best_val_test_post_conf_matrix': None,
-        'best_val_test_post_ece': None,
-        'best_val_test_post_adaece': None,
-        'best_val_test_post_cece': None,
-        'best_val_test_post_nll': None,
-        'best_val_test_post_all_cosM': None
+        'train_best_top1': None,
+        'val_best_top1': None,
+        'best_val_test_top1': None
     }
 
     net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
     cudnn.benchmark = True
 
     nll_criterion = nn.CrossEntropyLoss().cuda()
-    ece_criterion = ECELoss().cuda()
-    adaece_criterion = AdaptiveECELoss().cuda()
-    cece_criterion = ClasswiseECELoss().cuda()
-
     logfile = open('%s/test_log.txt' % (args.save_loc), 'w')
 
     for i in range(args.epoch):
@@ -448,35 +428,10 @@ if __name__ == "__main__":
             WH_relation_metric, H = compute_W_H_relation(W, mu_c_dict_train, mu_G_train, device)
             Wh_b_relation_metric = compute_Wh_b_relation(W, mu_G_train, b, device)
 
-            #         nf_metric_epoch = compute_nuclear_frobenius(before_class_dict_train)
-            avg_cos_margin, _ = compute_margin(before_class_dict_train, after_class_dict_train, W, b, mu_G_train,
-                                               device)
-
             info_dict['collapse_metric'].append(collapse_metric)
             info_dict['ETF_metric'].append(ETF_metric)
             info_dict['WH_relation_metric'].append(WH_relation_metric)
             info_dict['Wh_b_relation_metric'].append(Wh_b_relation_metric)
-
-            #             info_dict['mu_G_train'].append(mu_G_train.detach().cpu().numpy())
-            #             info_dict['mu_G_val'].append(mu_G_val.detach().cpu().numpy())
-            #             info_dict['mu_G_test'].append(mu_G_test.detach().cpu().numpy())
-            #             info_dict['mu_c_dict_train'].append(mu_c_dict_train)
-            #             info_dict['mu_c_dict_val'].append(mu_c_dict_val)
-            #             info_dict['mu_c_dict_test'].append(mu_c_dict_test)
-
-            #         info_dict['before_class_dict_train'].append(before_class_dict_train)
-            #         info_dict['after_class_dict_train'].append(after_class_dict_train)
-            #         info_dict['before_class_dict_val'].append(before_class_dict_val)
-            #         info_dict['after_class_dict_val'].append(after_class_dict_val)
-            #         info_dict['before_class_dict_test'].append(before_class_dict_test)
-            #         info_dict['after_class_dict_test'].append(after_class_dict_test)
-
-            #             info_dict['W'].append((W.detach().cpu().numpy()))
-            # if args.bias:
-            #             info_dict['b'].append(b.detach().cpu().numpy())
-
-            #         info_dict['nuclear_metric'].append(nf_metric_epoch)
-            info_dict['avg_cos_margin'].append(avg_cos_margin)
 
             info_dict['train_acc1'].append(train_acc1)
             info_dict['train_acc5'].append(train_acc5)
@@ -503,96 +458,24 @@ if __name__ == "__main__":
     if b is None:
         b = torch.zeros((W.shape[0],), device=device)
 
-    logits, labels = get_logits_labels(test_loader, net)
-    p_conf_matrix, p_accuracy, _, _, _ = test_classification_net_logits(logits, labels)
-
-    p_ece = ece_criterion(logits, labels).item()
-    p_adaece = adaece_criterion(logits, labels).item()
-    p_cece = cece_criterion(logits, labels).item()
-    p_nll = nll_criterion(logits, labels).item()
-
-    info_dict['best_val_test_prev_acc'] = p_accuracy
-    info_dict['best_val_test_prev_conf_matrix'] = p_conf_matrix
-    info_dict['best_val_test_prev_adaece'] = p_ece
-    info_dict['best_val_test_prev_cece'] = p_cece
-    info_dict['best_val_test_prev_nll'] = p_nll
-
-    p_mu_G_train, p_mu_c_dict_train, p_before_class_dict_train, p_after_class_dict_train, \
-    p_train_acc1, p_train_acc5 = compute_info(net, train_loader, device)
-
-    #     _, p_all_cos_margin = compute_margin(p_before_class_dict_train, p_after_class_dict_train, W, b, p_mu_G_train, device)
-
-    #     info_dict['best_val_test_prev_all_cosM'] = p_all_cos_margin
-
-    # res_str = '{:s}&{:.4f}&{:.4f}&{:.4f}&{:.4f}&{:.4f}'.format(saved_model_name,  1-p_accuracy,  p_nll,  p_ece,  p_adaece, p_cece)
-
-    # # Printing the required evaluation metrics
-    # if args.log:
-    #     print (p_conf_matrix)
-    #     print ('Test error: ' + str((1 - p_accuracy)))
-    #     print ('Test NLL: ' + str(p_nll))
-    #     print ('ECE: ' + str(p_ece))
-    #     print ('AdaECE: ' + str(p_adaece))
-    #     print ('Classwise ECE: ' + str(p_cece))
-
-    scaled_model = ModelWithTemperature(net, args.log)
-    scaled_model.set_temperature(val_loader, cross_validate=cross_validation_error)
-    T_opt = scaled_model.get_temperature()
-
-    b = None
-    for n, p in scaled_model.named_parameters():
-        if 'fc.weight' in n:
-            W = p
-        if 'fc.bias' in n:
-            b = p
-    if b is None:
-        b = torch.zeros((W.shape[0],), device=device)
-
-    logits, labels = get_logits_labels(test_loader, scaled_model)
-    conf_matrix, accuracy, _, _, _ = test_classification_net_logits(logits, labels)
-
-    ece = ece_criterion(logits, labels).item()
-    adaece = adaece_criterion(logits, labels).item()
-    cece = cece_criterion(logits, labels).item()
-    nll = nll_criterion(logits, labels).item()
-
-    info_dict['best_val_test_post_acc'] = accuracy
-    info_dict['best_val_test_post_conf_matrix'] = conf_matrix
-    info_dict['best_val_test_post_adaece'] = ece
-    info_dict['best_val_test_post_cece'] = cece
-    info_dict['best_val_test_post_nll'] = nll
-
     mu_G_train, mu_c_dict_train, before_class_dict_train, after_class_dict_train, \
-    train_acc1, train_acc5 = compute_info(scaled_model, train_loader, device)
+            train_acc1, train_acc5 = compute_info(net, train_loader, device)
 
-    #     _, all_cos_margin = compute_margin(before_class_dict_train, after_class_dict_train, W, b, mu_G_train,
-    #                                          device)
+    mu_G_val, mu_c_dict_val, before_class_dict_val, after_class_dict_val, \
+            val_acc1, val_acc5 = compute_info(net, val_loader, device)
 
-    #     info_dict['best_val_test_post_all_cosM'] = all_cos_margin
+    mu_G_test, mu_c_dict_test, before_class_dict_test, after_class_dict_test, \
+            test_acc1, test_acc5 = compute_info(net, test_loader, device)
+
+    info_dict['train_last_top1'] = train_acc1
+    info_dict['val_best_top1'] = val_acc1
+    info_dict['best_val_test_top1'] = test_acc1
+
     print_and_save(
-        '[Best Prev] | Top1: %.4f | Test NLL: %.4f | ECE: %.4f | AdaECE: %.4f | Classwise ECE: %.4f' %
-        (p_accuracy, p_nll, p_ece, p_adaece, p_cece), logfile)
-    print_and_save(
-        '[Best Post] | Top1: %.4f | Test NLL: %.4f | ECE: %.4f | AdaECE: %.4f | Classwise ECE: %.4f' %
-        (accuracy, nll, ece, adaece, cece), logfile)
-    print_and_save(p_conf_matrix, logfile)
-    print_and_save(conf_matrix, logfile)
-    print_and_save('Optimal temperature: ' + str(T_opt), logfile)
+        '[Best] | Train Top1: %.4f | Val Top1: %.4f | Test Top1: %.4f ' %
+        (train_acc1, val_acc1, test_acc1), logfile)
+
 
     logfile.close()
     with open(args.save_loc + '/info.pkl', 'wb') as f:
         pickle.dump(info_dict, f)
-
-    # res_str += '&{:.4f}({:.2f})&{:.4f}&{:.4f}&{:.4f}'.format(nll,  T_opt,  ece,  adaece, cece)
-
-    # if args.log:
-    #     print ('Optimal temperature: ' + str(T_opt))
-    #     print (conf_matrix)
-    #     print ('Test error: ' + str((1 - accuracy)))
-    #     print ('Test NLL: ' + str(nll))
-    #     print ('ECE: ' + str(ece))
-    #     print ('AdaECE: ' + str(adaece))
-    #     print ('Classwise ECE: ' + str(cece))
-    #
-    # # Test NLL & ECE & AdaECE & Classwise ECE
-    # print(res_str)
