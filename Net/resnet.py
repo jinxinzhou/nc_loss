3be6fa6b43d62de4fd04733dc9d27b0f0e715331
Jinxin Zhou <48702671+jinxinzhou@users.noreply.github.com>
2022-05-10 08:59:23 +0800

    Update resnet.py

--- Net/resnet.py
+++ Net/resnet.py
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1,width=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -37,20 +37,22 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, width=1):
         super(Bottleneck, self).__init__()
+        out_planes = self.expansion*planes
+        planes = int(planes * width)
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.conv3 = nn.Conv2d(planes, out_planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes)
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != out_planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes)
             )
 
     def forward(self, x):
@@ -63,19 +65,20 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, temp=1.0, etf=False, bias=True, **kwargs):
+    def __init__(self, block, num_blocks, width=1, num_classes=10, etf=False, bias=True, **kwargs):
         super(ResNet, self).__init__()
         self.in_planes = 64
+        self.width = width
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
+
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.fc = nn.Linear(512*block.expansion, num_classes, bias=bias)
         print('bias:', bias)
-        self.temp = temp
 
         for m in self.modules():
             if isinstance(m, nn.Linear):
@@ -92,7 +95,7 @@ class ResNet(nn.Module):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, self.width))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
@@ -104,35 +107,39 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         feature = out.view(out.size(0), -1)
-        out = self.fc(feature) / self.temp
+        out = self.fc(feature)
         return out, feature
 
 
-def resnet18(temp=1.0, **kwargs):
-    model = ResNet(BasicBlock, [2, 2, 2, 2], temp=temp, **kwargs)
+def resnet18(**kwargs):
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
     return model
 
 
-def resnet34(temp=1.0, **kwargs):
-    model = ResNet(BasicBlock, [3, 4, 6, 3], temp=temp, **kwargs)
+def resnet34(**kwargs):
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
     return model
 
 
-def resnet50(temp=1.0, **kwargs):
-    model = ResNet(Bottleneck, [3, 4, 6, 3], temp=temp, **kwargs)
+def resnet50(**kwargs):
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     return model
 
 
-def resnet101(temp=1.0, **kwargs):
-    model = ResNet(Bottleneck, [3, 4, 23, 3], temp=temp, **kwargs)
+def resnet101(**kwargs):
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     return model
 
 
-def resnet110(temp=1.0, **kwargs):
-    model = ResNet(Bottleneck, [3, 4, 26, 3], temp=temp, **kwargs)
+def resnet110(**kwargs):
+    model = ResNet(Bottleneck, [3, 4, 26, 3], **kwargs)
     return model
 
 
-def resnet152(temp=1.0, **kwargs):
-    model = ResNet(Bottleneck, [3, 8, 36, 3], temp=temp, **kwargs)
+def resnet152(**kwargs):
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return model
+
+def wide_resnet50_cifar(width=1, **kwargs):
+    model = ResNet(Bottleneck, [3, 4, 6, 3], width=width, **kwargs)
     return model
